1658d0744d87b2b9392c8085e4a5ea0aaf04ea21
victoralmeidamag <victorhugo.almeidamag@gmail.com>
2023-12-22 21:55:24 -0300

    ultimos ajustes

--- bird.py
+++ bird.py
@@ -1,10 +1,17 @@
-import pygame
+import pygame, random
 from pygame.locals import *
 
 LARGURA = 500
 ALTURA = 700
 VELOCIDADE = 10
 GRAVIDADE = 1
+VELOCIDADEJOGO = 10
+LARGURACHAO = 2 * LARGURA
+ALTURACHAO = 100
+TAMANHO_OBSTACULO = 500
+LARGURA_OBSTACULO = 80
+ESPACO_CANO = 200
+
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self):
@@ -15,7 +22,8 @@ class Bird(pygame.sprite.Sprite):
         
         self.velocidade = VELOCIDADE
         self.atualizaImg = 0
-        self.image = pygame.image.load('passaromeio.png').convert_alpha()
+        self.image = pygame.image.load('passarobaixo.png').convert_alpha()
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect[0] = LARGURA / 2
         self.rect[1] = ALTURA / 2
@@ -31,10 +39,46 @@ class Bird(pygame.sprite.Sprite):
         self.velocidade = -VELOCIDADE
 
 
-class base(pygame.sprite.Sprite):
-    def __init__(self, largura, altura):
-        self.image = pygame.image.load('base.png')
-        self.image = pygame.transform.scale(self.image(largura,altura))
-
+class Base(pygame.sprite.Sprite):
+    
+    def __init__(self, xpos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('base.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image,(LARGURACHAO,ALTURACHAO))
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
+        self.rect[0] = xpos
+        self.rect[1] = ALTURA - ALTURACHAO
 
+
+    def update(self):
+        self.rect[0] -= VELOCIDADEJOGO
+    
+def naTela(sprite):
+    return sprite.rect[0] < -(sprite.rect[2])
+
+def canosAleatorios(xpos):
+    tamanho = random.randint(100,300)
+    cano = Obstaculos(False, xpos, tamanho)
+    cano_invertido = Obstaculos(True, xpos, ALTURA - tamanho - ESPACO_CANO)
+    return(cano,cano_invertido)
+
+class Obstaculos(pygame.sprite.Sprite):
+    def __init__(self, inverte, xpos, tamanhoY):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load('pipe-green.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image,(LARGURA_OBSTACULO, TAMANHO_OBSTACULO))
+        self.rect = self.image.get_rect()
+        self.rect[0] = xpos
+
+        if inverte:
+            self.image = pygame.transform.flip(self.image,False, True)
+            self.rect[1] = -(self.rect[3] - tamanhoY)
+        else:
+            self.rect[1] = ALTURA - tamanhoY
+
+        self.mask = pygame.mask.from_surface(self.image)
+    
+    def update(self):
+        self.rect[0] -= VELOCIDADEJOGO
--- flappy.py
+++ flappy.py
@@ -1,9 +1,11 @@
 import pygame
 from pygame.locals import *
-from bird import Bird
+from bird import Bird, Base, Obstaculos, naTela, canosAleatorios
 
 LARGURA = 500
 ALTURA = 700
+LARGURACHAO = 2 * LARGURA
+ALTURACHAO = 100
 
 
 pygame.init()
@@ -15,6 +17,18 @@ BACKGROUND = pygame.transform.scale(BACKGROUND,(LARGURA, ALTURA))
 grupoPassaro = pygame.sprite.Group()
 passaro = Bird()
 grupoPassaro.add(passaro)
+grupoChao = pygame.sprite.Group()
+for i in range(2):
+    chao = Base(LARGURACHAO* i)
+    grupoChao.add(chao)
+
+
+grupoCano = pygame.sprite.Group()
+for i in range(2):
+    canos = canosAleatorios(LARGURA * i + 800)
+    grupoCano.add(canos[0])
+    grupoCano.add(canos[1])
+
 
 clock = pygame.time.Clock()
 
@@ -27,7 +41,32 @@ while True:
             if evento.key == K_SPACE:
                 passaro.saltar()
     tela.blit(BACKGROUND, (0, 0))
+
+    if naTela(grupoChao.sprites()[0]):
+        grupoChao.remove(grupoChao.sprites()[0]) 
+        novoChao = Base(LARGURACHAO - 20)
+        grupoChao.add(novoChao)
+
+    if naTela(grupoCano.sprites()[0]):
+        grupoCano.remove(grupoCano.sprites()[0])
+        grupoCano.remove(grupoCano.sprites()[0])     
+
+        canos = canosAleatorios(LARGURA * 2)
+        grupoCano.add(canos[0])
+        grupoCano.add(canos[1])
+
     grupoPassaro.update()
-    grupoPassaro.draw(tela)  
+    grupoChao.update()
+    grupoCano.update()
+
+
+    grupoPassaro.draw(tela)
+    grupoCano.draw(tela)
+    grupoChao.draw(tela)
+
+
     pygame.display.update()
+    
+    if pygame.sprite.groupcollide(grupoPassaro, grupoChao, False, False, pygame.sprite.collide_mask) or pygame.sprite.groupcollide(grupoPassaro, grupoCano, False, False, pygame.sprite.collide_mask):
+        break
 
